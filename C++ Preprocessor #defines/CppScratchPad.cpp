24eda84a12c11a9bd1da93030102f98253cd3dfa
Kulpot <kulpotwahu@gmail.com>
2024-07-06 13:46:56 +0800

    ..

--- C++ Preprocessor #defines/CppScratchPad.cpp
+++ C++ Preprocessor #defines/CppScratchPad.cpp
@@ -3,7 +3,7 @@ using std::cout;
 using std::endl;
 
 //C++ Preprocessor #defines   //https://www.youtube.com/watch?v=D_rgDGBB3_M&list=PLRwVmtr-pp06gII17UNrzeCGMMHnCPdOk
-//Stages to output and compile code:1st Stage(Preprocessor run - does a cut/paste), 2nd Stage
+//Stages to output and compile code:1st Stage(Preprocessor run - does a cut/paste), 2nd Stage(Compiler run - parses the file, generates the code to run,spits out .obj file that has code in it)
 
 void main()
 {
