c42d267e23db78ed4300d2300d995b83c216a349
Kulpot <kulpotwahu@gmail.com>
2024-07-07 12:54:49 +0800

    ..

--- C++ Preprocessor #defines/CppScratchPad.cpp
+++ C++ Preprocessor #defines/CppScratchPad.cpp
@@ -3,12 +3,12 @@ using std::cout;
 using std::endl;
 
 //C++ Preprocessor #defines   //https://www.youtube.com/watch?v=D_rgDGBB3_M&list=PLRwVmtr-pp06gII17UNrzeCGMMHnCPdOk
-//Stages to output and compile code:1st Stage(Preprocessor run - does a cut/paste), 2nd Stage(Compiler run - parses the file, generates the code to run,spits out .obj file that has code in it), 3rd Stage(The Linker - takes those obj files then does a fix-up and kicks out an .exe file or a .dll depending on the setup)
-//1st Step:Preprocessor ->	always start with the symbol "pound(#)" or "hash(#)", ex. #include <iostream> then cut/paste
+//Stages to output and compile code:1st Stage(Preprocessor run - does a copy/paste), 2nd Stage(Compiler run - parses the file, generates the code to run,spits out .obj file that has code in it), 3rd Stage(The Linker - takes those obj files then does a fix-up and kicks out an .exe file or a .dll depending on the setup)
+//1st Step:Preprocessor ->	always start with the symbol "pound(#)" or "hash(#)", ex. #include <iostream> then copy/paste
 //2nd Step:Compiler		
 //3rd Step:Linker
 
-#define PI 3.141592			// another sample of Preprocessor
+#define PI 3.1415927			// another sample of Preprocessor
 
 void main()
 {
